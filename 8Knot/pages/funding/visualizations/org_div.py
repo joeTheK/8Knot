cea78b39eb42b93df8a8c7472314bf98bd89c5f8
Joseph <joseph@thekelleys.org>
2023-12-06 07:09:12 -0600

    Spliced tog org diversity

--- 8Knot/pages/funding/visualizations/org_div.py
+++ 8Knot/pages/funding/visualizations/org_div.py
@@ -36,7 +36,13 @@ gc_org_div = dbc.Card(
                         dbc.PopoverHeader("Graph Info:"),
                         dbc.PopoverBody(
                             """
-                            Organizational diversity expresses how many different organizations are involved in a project and how involved different organizations are compared to one another. The more diversity within a project the more minds and coding styles are present. If you have one programmer from Amazon and one programmer from Exxon Mobile then you have two vastly different coding styles going into one project, for better or for worse.
+                            Organizational diversity expresses how many different organizations are involved in a project and how
+                              involved different organizations are compared to one another. The more diversity within a project the 
+                              more minds and coding styles are present. If you have one programmer from Amazon and one programmer from 
+                              Exxon Mobile then you have two vastly different coding styles going into one project, for better or for worse.
+                              There are two ways to gather data for this, one, by using whatever affiliation a contributor
+                              has in their GitHub bio, this way is less in-depth. Two, splicing together the most common
+                              email domains. You can select both these source with the radio options. (Company in bio coul take up to a minute to complete)
                             """
                         ),
                     ],
@@ -54,18 +60,18 @@ gc_org_div = dbc.Card(
                             [
                                 dbc.Label(
                                     "Date Interval:",
-                                    html_for=f"date-radio-{PAGE}-{VIZ_ID}",
+                                    html_for=f"data-radio-{PAGE}-{VIZ_ID}",
                                     width="auto",
                                 ),
                                 dbc.Col(
                                     [
                                         dbc.RadioItems(
-                                            id=f"date-radio-{PAGE}-{VIZ_ID}",
+                                            id=f"data-radio-{PAGE}-{VIZ_ID}",
                                             options=[
                                                 {"label": "Company in Bio", "value": "C"},
-                                                {"label": "Email Domain", "value": "D"},
+                                                {"label": "Email Domain", "value": "E"},
                                             ],
-                                            value="C",
+                                            value="E",
                                             inline=True,
                                         ),
                                     ]
@@ -146,13 +152,14 @@ def toggle_popover(n, is_open):
     Output(f"{PAGE}-{VIZ_ID}", "figure"),
     [
         Input("repo-choices", "data"),
+        Input(f"data-radio-{PAGE}-{VIZ_ID}", "value"),
         Input(f"contributions-required-{PAGE}-{VIZ_ID}", "value"),
         Input(f"date-picker-range-{PAGE}-{VIZ_ID}", "start_date"),
         Input(f"date-picker-range-{PAGE}-{VIZ_ID}", "end_date"),
     ],
     background=True,
 )
-def organizatin_diversity_graph(repolist, num, start_date, end_date):
+def organizatin_diversity_graph(repolist, data_source, num, start_date, end_date):
     # wait for data to asynchronously download and become available.
     cache = cm()
     df = cache.grabm(func=cmq, repos=repolist)
@@ -168,16 +175,78 @@ def organizatin_diversity_graph(repolist, num, start_date, end_date):
         logging.warning(f"{VIZ_ID} - NO DATA AVAILABLE")
         return nodata_graph
 
+    # if data source is for gh based company name use gh_affiliation graph
     # function for all data pre processing, COULD HAVE ADDITIONAL INPUTS AND OUTPUTS
-    df = process_data(df, num, start_date, end_date)
-
-    fig = create_figure(df)
+    if data_source == "C":
+        df = process_data_gh_affil(df, num, start_date, end_date)
+        fig = create_figure_gh_affil(df)
+    
+    # if data source is for company email domain
+    if data_source == "E":
+        df = process_data_email(df, num, start_date, end_date)
+        fig = create_figure_email(df)
 
     logging.warning(f"{VIZ_ID} - END - {time.perf_counter() - start}")
     return fig
 
+## MAKE FIGURE FOR EMAIL from /affiliation/commit_domain
+def process_data_email(df: pd.DataFrame, num, start_date, end_date):
+    # convert to datetime objects rather than strings
+    df["created"] = pd.to_datetime(df["created"], utc=True)
 
-def process_data(df: pd.DataFrame, num, start_date, end_date):
+    # order values chronologically by COLUMN_TO_SORT_BY date
+    df = df.sort_values(by="created", axis=0, ascending=True)
+
+    # filter values based on date picker
+    if start_date is not None:
+        df = df[df.created >= start_date]
+    if end_date is not None:
+        df = df[df.created <= end_date]
+
+    # creates list of unique emails and flattens list result
+    emails = df.email_list.str.split(" , ").explode("email_list").unique().tolist()
+
+    # remove any entries not in email format
+    emails = [x for x in emails if "@" in x]
+
+    # creates list of email domains from the emails list
+    email_domains = [x[x.rindex("@") + 1 :] for x in emails]
+
+    # creates df of domains and counts
+    df = pd.DataFrame(email_domains, columns=["domains"]).value_counts().to_frame().reset_index()
+
+    df = df.rename(columns={0: "occurences"})
+
+    # changes the name of the company if under a certain threshold
+    df.loc[df.occurences <= num, "domains"] = "Other"
+
+    # groups others together for final counts
+    df = (
+        df.groupby(by="domains")["occurences"]
+        .sum()
+        .reset_index()
+        .sort_values(by=["occurences"], ascending=False)
+        .reset_index(drop=True)
+    )
+
+    return df
+
+
+def create_figure_email(df: pd.DataFrame):
+    # graph generation
+    fig = px.pie(df, names="domains", values="occurences", color_discrete_sequence=color_seq)
+    fig.update_traces(
+        textposition="inside",
+        textinfo="percent+label",
+        hovertemplate="%{label} <br>Contributions: %{value}<br><extra></extra>",
+    )
+
+    return fig
+
+
+## MAKE FIGURE FOR GH_AFFILIATION from /affiliation/gh_affiliation
+
+def process_data_gh_affil(df: pd.DataFrame, num, start_date, end_date):
     """Implement your custom data-processing logic in this function.
     The output of this function is the data you intend to create a visualization with,
     requiring no further processing."""
@@ -252,7 +321,7 @@ def fuzzy_match(df, name):
     return [i for i, x in enumerate(matches) if x]
 
 
-def create_figure(df: pd.DataFrame):
+def create_figure_gh_affil(df: pd.DataFrame):
     # graph generation
     fig = px.pie(
         df,
